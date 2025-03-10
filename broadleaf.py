d517f6cb3999e1dfc93d832f8fd47fcf7525612f
ajoe2 <ajoe2468@gmail.com>
2025-03-10 12:02:26 -0700

    Add rate item transaction simulation

--- broadleaf.py
+++ broadleaf.py
@@ -2,8 +2,12 @@
 import numpy as np
 from transaction import Transaction
 
+#################################
+####   Simulator functions   ####
+#################################
+
 ### Transaction 1 ###
-def doFilterInternalUnlessIgnored(request, response, chain):
+def doFilterInternalUnlessIgnored(request: tuple[int, int], response, chain):
     """
     Purpose: Update cart with new order
     Source code: https://github.com/BroadleafCommerce/BroadleafCommerce/blob/develop-7.0.x/core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/payment/service/OrderPaymentServiceImpl.java#L106C5-L149C6
@@ -24,24 +28,19 @@ def doFilterInternalUnlessIgnored(request, response, chain):
     """
     cart_id, order_id = request[0], request[1]
     t = Transaction()
-    t.append_read(cart_id)
-    t.append_write(order_id)
+    t.append_read(f"cart({cart_id})")
+    t.append_write(f"order({order_id})")
     return t
 
 def broadleaf_update_order_sim(num_transactions: int):
     """
     Example output:
 
-    ['r-5', 'w-7']
-    ['r-76', 'w-55']
-    ['r-3', 'w-49']
-    ['r-95', 'w-73']
-    ['r-80', 'w-37']
-    ['r-40', 'w-99']
-    ['r-17', 'w-55']
-    ['r-82', 'w-78']
-    ['r-36', 'w-95']
-    ['r-7', 'w-40']
+    ['r-cart(23)', 'w-order(87)']
+    ['r-cart(85)', 'w-order(89)']
+    ['r-cart(19)', 'w-order(36)']
+    ['r-cart(96)', 'w-order(9)']
+    ['r-cart(77)', 'w-order(23)']
     """
     num_carts = 100
     num_orders = 100
@@ -56,12 +55,75 @@ def broadleaf_update_order_sim(num_transactions: int):
         print(transaction)
 
 ### Transaction 2 ###
-def transaction_2():
-    # TODO
-    pass
+def rateItem(itemId, type, customer, rating):
+    """
+    Purpose: Add a new rating to item
+    Github: https://github.com/BroadleafCommerce/BroadleafCommerce/blob/develop-7.0.x/core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/rating/service/RatingServiceImpl.java#L73C1-L92C6
+    
+    Pseudocode:
+        In: ratings_summary, ratings_detail
+
+        TRANSACTION START
+        summary = SELECT * FROM ratings_summary WHERE itemID = itemID AND type = type
+        if !summary:
+            create_summary(itemID, type)
+        detail = SELECT * FROM ratings_detail WHERE customer_ID = customer.id, rating_id = summary.id
+        if !detail:
+            create_detail(customer.id, summary.id)
+        UPDATE ratings_detail SET rating = rating WHERE customer_ID = customer.id, rating_id = summary.id
+        INSERT INTO ratings_summary VALUES (itemID, type, rating, date, customer.id)
+        TRANSACTION COMMIT
+    
+    For simplicity, we treat the itemID as the unique identifier for the item. 
+    """
+    t = Transaction()
+    t.append_read(f"summary({itemId})")
+    t.append_read(f"detail({customer})")
+    t.append_write(f"detail({customer})/rating({rating})")
+    t.append_write(f"summary({itemId})/rating({rating})")
+    return t
+
+def rateItem_sim(num_transactions: int):
+    """
+    Example output:
+
+    ['r-summary(80)', 'r-detail(57)', 'w-detail(57)/rating(3)', 'w-summary(80)/rating(3)']
+    ['r-summary(80)', 'r-detail(46)', 'w-detail(46)/rating(9)', 'w-summary(80)/rating(9)']
+    ['r-summary(72)', 'r-detail(10)', 'w-detail(10)/rating(5)', 'w-summary(72)/rating(5)']
+    ['r-summary(76)', 'r-detail(1)', 'w-detail(1)/rating(5)', 'w-summary(76)/rating(5)']
+    ['r-summary(34)', 'r-detail(43)', 'w-detail(43)/rating(5)', 'w-summary(34)/rating(5)']
+    """
+    num_items = 100
+    num_customers = 100
+    ratings = range(10)
+    for _ in range(num_transactions):
+        transaction = rateItem(np.random.choice(range(num_items)),
+                               None,
+                               np.random.choice(range(num_customers)),
+                               np.random.choice(ratings))
+        print(transaction)
+
+#######################
+####   Simulation  ####
+#######################
 
 def main():
-    broadleaf_update_order_sim(10) # Transaction 1
-    
+    """
+    Generate transaction traces for Broadleaf.
+    """
+    # Transaction 1
+    num_transactions_1 = 5
+    print(f"Generating Broadleaf update cart simulation...")
+    broadleaf_update_order_sim(num_transactions_1)
+    print()
+
+    # Transaction 2
+    num_transactions_2 = 5
+    print(f"Generating Broadleaf rate item simulation...")
+    rateItem_sim(num_transactions_2)
+    print()
+
+    # More transactions (TODO)
+
 if __name__ == "__main__":
     main()
