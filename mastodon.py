1dc67cea5a86b276da790d4f7d8826b91cb2b486
ajoe2 <ajoe2468@gmail.com>
2025-04-07 20:19:29 -0700

    first commit

---
+++ mastodon.py
@@ -0,0 +1,38 @@
+"""
+Transaction simulations for Mastodon: https://github.com/mastodon/mastodon/tree/main
+Uses PostgreSQL
+Analyzed in Tang et al. Ad Hoc Transactions in Web Applications: The Good, 
+the Bad, and the Ugly: https://ipads.se.sjtu.edu.cn/_media/publications/concerto-sigmod22.pdf
+
+### EXAMPLE OUTPUT ###
+
+---
+"""
+
+import numpy as np
+from transaction import Transaction
+
+#################################
+####   Simulator functions   ####
+#################################
+
+### Transaction 1 ###
+
+#######################
+####   Simulation  ####
+#######################
+
+def main():
+    """
+    Generate Mastodon transaction traces
+    """
+    num_transactions_1 = 5
+
+    # Extra space for formatting
+    print()
+
+    # Transaction 1
+    print()
+
+if __name__ == "__main__":
+    main()
